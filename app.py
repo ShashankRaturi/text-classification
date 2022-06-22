48b55b055f7244f4f302adb96d20beac48b30eb6
shashank <shashankraturi4@gmail.com>
2022-06-22 18:23:24 +0530

    improved frontend

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ import predict
 app = Flask(__name__)
 
 
+categories = [
+    'alt.atheism',
+    'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'comp.windows.x',
+    'misc.forsale',
+    'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey',
+    'sci.crypt', 'sci.electronics', 'sci.med', 'sci.space',
+    'soc.religion.christian',
+    'talk.politics.guns', 'talk.politics.mideast', 'talk.politics.misc', 'talk.religion.misc'
+]
+
+
 @app.route('/' , methods=['GET' , 'POST'])
 def hello():
 
@@ -13,7 +24,9 @@ def hello():
         predicted_category = predict.predict_categ(incoming_text)
         print(predicted_category)
  
-    return render_template('index.html' , p = predicted_category)
+    return render_template('index.html' , categories=categories , p_category = predicted_category)
+
+
 
 # @app.route('/sub' , methods = ['POST'])
 # def submit():
